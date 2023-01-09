e901b0ea078b31f2060a3ed46e25c3a5226c4f9a
Zeratul4587 <margaritovkl@gmail.com>
2023-01-09 22:16:39 +0000

    commiting 06 Task

---
+++ 05 Task/Program.cs
@@ -0,0 +1,17 @@
+﻿//Декларирайте променлива от тип char и присвоете като стойност
+//символа, който има Unicode код 72 (използвайте калкулатора на
+//Windows, за да намерите шестнайсетичното представяне на 72).
+//the Unicode of tipe char and the solution of task 5 is  U + 0048
+using System.Diagnostics.SymbolStore;
+
+namespace _05_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char symbol = 'H';
+            Console.WriteLine(symbol);
+        }
+    }
+}
---
+++ 06 Task/Program.cs
@@ -0,0 +1,16 @@
+﻿//Декларирайте променлива isMale от тип bool и присвоете стойност на 
+//последната в зависимост от вашия пол.
+
+namespace _06_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+            //string concratination example, below
+
+            Console.WriteLine("isMale: " + isMale);
+        }
+    }
+}
