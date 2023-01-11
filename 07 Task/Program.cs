e09ad5053384fcd3ef0e9b81fec325795520982b
Zeratul4587 <margaritovkl@gmail.com>
2023-01-11 18:58:57 +0000

    07 Task

---
+++ 07 Task/Program.cs
@@ -0,0 +1,21 @@
+﻿//Декларирайте две променливи от тип string със стойности "Hello" и 
+//"World".
+//Декларирайте променлива от тип object.
+//Присвоете на тази 
+//променлива стойността, която се получава от конкатенацията на двете 
+//стрингови променливи (добавете интервал, ако е необходимо).
+//Отпечатайте променливата от тип object.
+
+namespace _07_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting1 = "Hello";
+            string greeting2 = "World";
+            object mainGreeting = greeting1 + " " + greeting2;
+            Console.WriteLine(mainGreeting);
+        }
+    }
+}
