dd7aa1a6501c0fcae5493f7039ff4170d1a34fcd
Zeratul4587 <margaritovkl@gmail.com>
2023-01-12 21:19:52 +0000

    13 Task

---
+++ 13 Task/Program.cs
@@ -0,0 +1,20 @@
+﻿//Декларирайте две променливи от тип int.
+//Задайте им стойности съответно 5 и 10.
+//Разменете стойностите им
+//и ги отпечатайте.
+namespace _13_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = 5;
+            int secondNumber = 10;
+            //swoapimg first number and second number above
+            int firstTempNumber = secondNumber;
+            int secondTempNumber = firstNumber;
+
+            Console.WriteLine(firstTempNumber + " " + secondTempNumber);
+        }
+    }
+}
