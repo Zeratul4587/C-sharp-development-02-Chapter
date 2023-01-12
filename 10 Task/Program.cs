a23055ba7a5fe1f7fd4d1719cfe608890d46cebe
Zeratul4587 <margaritovkl@gmail.com>
2023-01-12 19:03:21 +0000

    10 Task

---
+++ 10 Task/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишете програма,
+//която принтира фигура във формата на сърце със 
+//знака "o".
+
+namespace _10_Task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   0   0   ");
+            Console.WriteLine(" o   o   o ");
+            Console.WriteLine("  o     o  ");
+            Console.WriteLine("   o   o   ");
+            Console.WriteLine("     o     ");
+        }
+    }
+}
