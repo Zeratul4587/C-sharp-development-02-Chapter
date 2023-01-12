c032cc96aeeeb6c7d26bb0e8761890ca81fe574e
Zeratul4587 <margaritovkl@gmail.com>
2023-01-12 19:58:53 +0000

    11 Task

---
+++ 11 Task/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишете програма, която принтира на конзолата равнобедрен
+//триъгълник,
+//като страните му са очертани от символа "©".
+//solution https://www.youtube.com/watch?v=rTqBnJ8HrSc 
+namespace _11_Task
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("\x00A9");
+
+            Console.WriteLine("             ©               ");
+            Console.WriteLine("            © ©              ");
+            Console.WriteLine("           ©   ©             ");
+            Console.WriteLine("          ©     ©            ");
+            Console.WriteLine("         ©   ©   ©           ");
+        }
+    }
+    
+    
+}
